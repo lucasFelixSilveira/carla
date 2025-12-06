9d0b3b98fe14caec2593b447b6e7ff4b0482e5b4
lucasFelixSilveira <lucasdwbfff@gmail.com>
2025-12-05 22:44:38 -0300

    making pre-compiled types on the lambda arguments

--- compiler/parser/patterns/lambda.hpp
+++ compiler/parser/patterns/lambda.hpp
@@ -2,13 +2,14 @@
 
 #include "../ast.hpp"
 #include "../symbols.hpp"
+#include <cstddef>
 #include <memory>
 #include <variant>
 #include <vector>
 
+#include "components/special_symbols.hpp"
 
 bool lambda(pNode *result, Symt *sym, long unsigned int *index, const std::vector<pContext>* ctx) {
-    std::vector<pContext> specialCtx;
 
     pContext arguments = (*ctx)[*index];
     if(! std::holds_alternative<std::vector<pContext>>(arguments.content) ) return false;
@@ -22,6 +23,7 @@ bool lambda(pNode *result, Symt *sym, long unsigned int *index, const std::vecto
     size_t internal = 0;
 
     while(internal < blockContent.size()) {
+        std::vector<pContext> specialCtx;
         if( internal + 1 >= blockContent.size() ) return false;
 
         Symbol* type;
@@ -31,15 +33,20 @@ bool lambda(pNode *result, Symt *sym, long unsigned int *index, const std::vecto
         if( (type = sym->findSymbol(typeTk.lexeme)) == nullptr ) return false;
         if(! (std::holds_alternative<std::shared_ptr<morgana::type>>(*type) || std::holds_alternative<std::shared_ptr<special>>(*type)) ) return false;
 
-        const pContext& idCtx = blockContent[internal + 1];
+        unsigned long int *sext = &internal;
+        int jumped = check_special(type, sym, &blockContent, sext, specialCtx) ? 1 : 0;
+
+        const pContext& idCtx = blockContent[internal + jumped + 1];
         if( idCtx.kind != Common || !std::holds_alternative<Token>(idCtx.content) ) return false;
         Token idTk = std::get<Token>(idCtx.content);
         if( idTk.kind != IDENTIFIER || idTk.lexeme.empty() ) return false;
 
-        argst.push_back(std::get<std::shared_ptr<morgana::type>>(*type));
+        std::shared_ptr<morgana::type> into = builtin(type, assemble_special_symbol(sym, specialCtx));
+
+        argst.push_back(into);
         argsn.push_back(idTk.lexeme);
 
-        internal += 2;
+        internal += 2 + jumped;
         if( internal >= blockContent.size() ) break;
 
         const pContext& symbol = blockContent[internal];
